5d180353845e3d62ff82e3fe53c04ce365dec679
johnbuenodev <johnbuenodev@gmail.com>
2023-08-12 16:11:09 -0300

    Iniciando Dashboard com vue 3. criado componente login(inicial)

---
+++ src/components/DashBoardPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>DashBoard</div>
+</template>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginPage from '../components/LoginPage.vue'
+import DashBoardPage from '../components/DashBoardPage.vue'
 
 const routes = [
   {
@@ -8,12 +10,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/dashboard',
+    name: 'home',
+    component: DashBoardPage
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage
   }
 ]
 
